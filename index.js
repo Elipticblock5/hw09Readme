e0973eb8f21d7c7fdc71002cba0a02deef3548de
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-04-04 13:10:58 -0700

    added dependencies

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
 
     {
         type: "input",
+        name: "email",
+        message: "Please enter your email address?"
+    },
+
+    {
+        type: "input",
         name: "github",
         message: "Please enter your Github username?"
-    }
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return.fs.writeFile(path.join(process.cwd(), fileName), data);
+    return fs.writeFile(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();
+init() {
+    inquirer.prompt(questions)
+    .then((questionAnswers) => {
+        console.log("Making your readme...hold on a second...");
+        writeToFile("README.md", generateMarkdown({...questionAnswers}));
+    })
+};
