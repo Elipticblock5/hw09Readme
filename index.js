a78259d2346f51f93955e096d2084ea65d21f030
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-04-04 12:59:03 -0700

    installed npm

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your Github username?"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return.fs.writeFile(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
