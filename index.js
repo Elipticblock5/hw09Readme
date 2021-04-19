871adb9a663291ab8bee5662932342e8f448f12f
NATE JOHNSON <42559281+Elipticblock5@users.noreply.github.com>
2021-04-19 12:52:05 -0700

    questions working, functions not

--- index.js
+++ index.js
@@ -18,7 +18,51 @@ const questions = [
         name: "github",
         message: "Please enter your Github username?"
     },
-];
+
+    {
+        type: "input",
+        name: "Project_Title",
+        message: "Please enter your project's name"
+    },
+
+    {
+        type: "list",
+        message: "What license is the project currently under?",
+        name: "license",
+        choices: [
+            "Unlicensed",
+            "Mozilla Public License 2.0",
+            "MIT",
+            "GNU",
+        ]
+    },
+
+    {
+        type: "input",
+        message: "What information would you like to add about contributing?",
+        name: "contributions"
+    },
+
+    {
+        type: "input",
+        message: "Any special instructions on using repository?",
+        name: "usage",
+    },
+
+    {
+        type: "input",
+        message: "Can you shortly describe your project?",
+        name: "description",
+    },
+
+    {
+        type: "input",
+        message: "what command is used to run tests?",
+        name: "tests",
+        default: "npm run test",
+    },
+
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
